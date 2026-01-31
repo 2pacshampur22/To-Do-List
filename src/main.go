7cd7056550a5d163c4761ea9cf29e30bda18dcf3
2pac_shampur22 <daokws@mail.ru>
2026-01-31 13:58:31 +0300

    first

---
+++ src/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"To-Do-List/storage"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	store := storage.NewStorage("tasks.json")
+
+	var input string
+	fmt.Println("Please provide an input argument: add, list, done, delete")
+	fmt.Println("If you want to add a task, provide name and description after add command using \"\" for both.")
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide an input argument.")
+		return
+	}
+	input = os.Args[1]
+
+	switch input {
+	case "add":
+		if len(os.Args) < 4 {
+			fmt.Println("Please provide task name and description.")
+			return
+		}
+		name := os.Args[2]
+		description := os.Args[3]
+		err := store.Add(storage.Task{
+			Name:        name,
+			Description: description,
+			IsDone:      false,
+		})
+		if err != nil {
+			fmt.Printf("Error adding task: %v\n", err)
+			return
+		}
+		fmt.Println("Task added successfully")
+	case "list":
+		tasks, err := store.List()
+		if err != nil {
+			fmt.Printf("Error listing tasks: %v\n", err)
+			return
+		}
+		for _, task := range tasks {
+			fmt.Printf("Task: %+v\n", task)
+		}
+
+	case "done":
+		if len(os.Args) < 3 {
+			fmt.Println("Please provide task ID to mark as done.")
+			return
+		}
+		id := os.Args[2]
+		idInt, err := strconv.Atoi(id)
+		if err != nil {
+			fmt.Println("Invalid task ID:", id)
+			return
+		}
+
+		err = store.Done(idInt)
+		if err != nil {
+			fmt.Printf("Error marking task as done: %v\n", err)
+			return
+		}
+		fmt.Printf("Marked task with ID %d as done\n", idInt)
+
+	case "delete":
+		if len(os.Args) < 3 {
+			fmt.Println("Please provide task ID to delete.")
+			return
+		}
+		id := os.Args[2]
+		idInt, err := strconv.Atoi(id)
+		if err != nil {
+			fmt.Println("Invalid task ID:", id)
+			return
+		}
+
+		err = store.Delete(idInt)
+		if err != nil {
+			fmt.Printf("Error deleting task: %v\n", err)
+			return
+		}
+		fmt.Printf("Deleted task with id: %v", id)
+
+	default:
+		fmt.Println("Unknown command, please use add, list, done, delete or update")
+	}
+}
---
+++ storage/storage.go
@@ -0,0 +1,138 @@
+package storage
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Task struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	IsDone      bool   `json:"isDone"`
+}
+
+type Storage struct {
+	Path string
+}
+
+func NewStorage(path string) *Storage {
+	return &Storage{Path: path}
+}
+
+func (s *Storage) LoadTasks() ([]Task, error) {
+	var tasks []Task
+	taskFile, err := os.ReadFile(s.Path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return tasks, nil
+		}
+		return nil, err
+	}
+	err = json.Unmarshal(taskFile, &tasks)
+	if err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
+
+func (s *Storage) SaveTasks(tasks []Task) error {
+	tasksByte, err := json.MarshalIndent(tasks, "", " ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(s.Path, tasksByte, 0644)
+}
+
+func (s *Storage) Add(t Task) error {
+	var maxId int
+	tasks, err := s.LoadTasks()
+	if err != nil {
+		return err
+	}
+
+	for _, task := range tasks {
+		if task.ID > maxId {
+			maxId = task.ID
+		}
+	}
+
+	t.ID = maxId + 1
+	tasks = append(tasks, t)
+	err = s.SaveTasks(tasks)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Storage) List() ([]Task, error) {
+	tasks, err := s.LoadTasks()
+	if err != nil {
+		return nil, fmt.Errorf("Failed to load tasks: %v", err)
+	}
+
+	if len(tasks) == 0 {
+		return nil, fmt.Errorf("No tasks found")
+	}
+
+	return tasks, nil
+}
+
+func (s *Storage) Done(id int) error {
+	tasks, err := s.LoadTasks()
+	if err != nil {
+		return fmt.Errorf("Error in loading tasks: %v", err)
+	}
+
+	if len(tasks) == 0 {
+		return fmt.Errorf("No tasks found")
+	}
+
+	found := false
+	for i, task := range tasks {
+		if task.ID == id {
+			tasks[i].IsDone = true
+			err = s.SaveTasks(tasks)
+			if err != nil {
+				return fmt.Errorf("Error saving tasks: %v", err)
+			}
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		return fmt.Errorf("Task ID not found")
+	}
+	return nil
+}
+
+func (s *Storage) Delete(id int) error {
+	tasks, err := s.LoadTasks()
+	if err != nil {
+		return err
+	}
+
+	if len(tasks) == 0 {
+		return fmt.Errorf("No tasks found")
+	}
+
+	found := false
+	for i, task := range tasks {
+		if task.ID == id {
+			found = true
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			err = s.SaveTasks(tasks)
+			if err != nil {
+				return fmt.Errorf("Error saving tasks: %v", err)
+			}
+			break
+		}
+	}
+	if !found {
+		return fmt.Errorf("Task ID not found")
+	}
+	return nil
+}
